50ca705db7295a474da3e542bd70feff8267721a
Jack Franklin <jack@jackfranklin.net>
2012-04-29 18:52:12 +0100

    end of tutorial1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes/routes');
 
 var app = module.exports = express.createServer();
 
@@ -30,7 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
+app.get('/about', routes.about);
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.about = function(req, res){
+  res.render('index', { title: 'Something Else' })
+};
