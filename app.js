7a8911551e493847dfb6ea8861999bfb4b4892b4
Jack Franklin <jack@jackfranklin.net>
2012-05-03 12:46:37 +0100

    started on the actual todo app now

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.get('/about', routes.about);
+app.get('/todo', routes.todo);
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,24 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Welcome to JSPlayground Todo' });
 };
 
 exports.about = function(req, res){
-  res.render('index', { title: 'Something Else' })
+  res.render('index', { title: 'Something Else' });
+};
+
+exports.todo = function(req, res){
+  res.render('todo', {
+    title: 'New Todo List',
+    todos: [
+      { text: "todo number 1" },
+      { text: "todo number 2" }
+    ]
+  });
+};
+
+exports.saveTodo = function(req, res) {
+
+
 };
